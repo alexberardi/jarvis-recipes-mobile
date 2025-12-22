06a88e88d2dde6bd86343ee52614c99402ebbf2c
alexberardi <alexmberardi@gmail.com>
2025-12-22 13:52:36 -0500

    app version

--- app.config.js
+++ app.config.js
@@ -17,7 +17,6 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.jarvisautomation.jarvisrecipesmobile',
-      buildNumber: '8', // Start at 8 since last uploaded was 7, autoIncrement will handle future builds
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
